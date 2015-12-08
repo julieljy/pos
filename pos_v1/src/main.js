f88d6094654d427f42e8ccafcfbd5158b4fb8e9d
julieljy <julieljy0328@gmail.com>
2015-12-08 16:00:16 +0800

    reconstitute functions

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -7,15 +7,43 @@ var items_buy_count_total=[];
 var all_total=[];
 var result={};
 
+
+function get_weight(a,items_buy,inputs){
+    var need_weight=[];
+    need_weight=inputs[a].split('-');
+    for(var k=0;k<all_items.length;k++){
+        if(need_weight[0]==all_items[k].barcode){
+            for(var l=0;l<parseInt(need_weight[1]);l++){
+                items_buy.push(all_items[k]);
+            }
+        }
+    }
+}
+
 function get_items_buy(inputs,all_items){
-    var n=0;
     var check;
     for(var i=0;i<inputs.length;i++){
         check=0;
         for(var j=0;j<all_items.length;j++){
             if(inputs[i]==all_items[j].barcode){
-                items_buy[n]=all_items[j];
-                n++;
+                items_buy.push(all_items[j]);
+                check=1;
+            }
+        }
+        if(check==0){
+            get_weight(i,items_buy,inputs);
+        }
+    }
+    return items_buy;
+}
+/*
+function get_items_buy(inputs,all_items){
+    var check;
+    for(var i=0;i<inputs.length;i++){
+        check=0;
+        for(var j=0;j<all_items.length;j++){
+            if(inputs[i]==all_items[j].barcode){
+                items_buy.push(all_items[j]);
                 check=1;
             }
         }
@@ -25,8 +53,7 @@ function get_items_buy(inputs,all_items){
             for(var k=0;k<all_items.length;k++){
                 if(need_weight[0]==all_items[k].barcode){
                     for(var l=0;l<parseInt(need_weight[1]);l++){
-                        items_buy[n]=all_items[k];
-                        n++;
+                    items_buy.push(all_items[k]);
                     }
                 }
             }
@@ -34,51 +61,50 @@ function get_items_buy(inputs,all_items){
     }
     return items_buy;
 }
+*/
+
+function items_count(a,items_buy_count,items_buy){
+    check=0;
+    for(var j=0;j<items_buy_count.length;j++){
+        if(items_buy[a].barcode==items_buy_count[j].barcode){
+           items_buy_count[j].count++;
+           check=1;
+        }
+    }
+    if(check==0){
+        items_buy_count.push({barcode:items_buy[a].barcode,
+                              name:items_buy[a].name,
+                              count:1,
+                              unit:items_buy[a].unit,
+                              price:items_buy[a].price});
+    }
+}
+
 
 function get_items_buy_count(items_buy){
     //items_buy=get_items_buy();
     var check;
-    var n=0;
-    items_buy_count[0]={barcode:items_buy[0].barcode,
+    items_buy_count.push({barcode:items_buy[0].barcode,
                         name:items_buy[0].name,
                         count:1,
                         unit:items_buy[0].unit,
-                        price:items_buy[0].price};
+                        price:items_buy[0].price});
     for(var i=1;i<items_buy.length;i++){
-        check=0;
-        for(var j=0;j<items_buy_count.length;j++){
-            if(items_buy[i].barcode==items_buy_count[j].barcode){
-                items_buy_count[j].count++;
-                check=1;
-            }
-        }
-
-        if(check==0){
-
-            n++;
-            items_buy_count[n]={barcode:items_buy[i].barcode,
-                                name:items_buy[i].name,
-                                count:1,
-                                unit:items_buy[i].unit,
-                                price:items_buy[i].price};
-        }
-
+        items_count(i,items_buy_count,items_buy);
     }
     return items_buy_count;
 }
 
 function get_items_free(items_buy_count,promotions){
     //items_buy_count=get_items_buy_count();
-    var n=0;
     for(var i=0;i<items_buy_count.length;i++){
         for(var j=0;j<promotions[0].barcodes.length;j++){
             if(items_buy_count[i].barcode==promotions[0].barcodes[j]){
                 if(items_buy_count[i].count>=3){
-                    items_free[n]={name:items_buy_count[i].name,
+                    items_free.push({name:items_buy_count[i].name,
                                    count:(items_buy_count[i].count-items_buy_count[i].count%3)/3,
                                    unit:items_buy_count[i].unit,
-                                   price:items_buy_count[i].price};
-                    n++;
+                                   price:items_buy_count[i].price});
                 }
             }
         }
@@ -86,21 +112,26 @@ function get_items_free(items_buy_count,promotions){
     return items_free;
 }
 
+function items_free_total(a,items_buy_count_total,items_buy_count,items_free){
+    check=0;
+    items_buy_count_total[a]=items_buy_count[a];
+    for(var j=0;j<items_free.length;j++){
+        if(items_buy_count[a].name==items_free[j].name){
+            items_buy_count_total[a].total=(items_buy_count[a].count-items_free[j].count)*items_buy_count[a].price;
+            check=1;
+        }
+    }
+    if(check==0){
+        items_buy_count_total[a].total=items_buy_count[a].count*items_buy_count[a].price;
+    }
+
+}
+
 function get_items_buy_count_total(items_buy_count,items_free){
     //items_buy_count=get_items_buy_count();
     var check;
     for(var i=0;i<items_buy_count.length;i++){
-        check=0;
-        items_buy_count_total[i]=items_buy_count[i];
-        for(var j=0;j<items_free.length;j++){
-            if(items_buy_count[i].name==items_free[j].name){
-                items_buy_count_total[i].total=(items_buy_count[i].count-items_free[j].count)*items_buy_count[i].price;
-                check=1;
-            }
-        }
-        if(check==0){
-            items_buy_count_total[i].total=items_buy_count[i].count*items_buy_count[i].price;
-        }
+        items_free_total(i,items_buy_count_total,items_buy_count,items_free);
     }
     return items_buy_count_total;
 }
